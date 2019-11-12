c1211c3c781eafe7cbc9c193c866498a6941a9e7
hirokinose <zm18227@josai.ac.jp>
2019-11-12 03:40:55 +0000

    11/12

---
+++ ex6-5.js
@@ -0,0 +1,14 @@
+
+var who65 = 'world' ;
+// var come = 'が来た'
+function sayhello65(){
+// var who = 'world';
+ alert ('Helloworld, ' + '!');
+}
+
+function hiroki65(){
+    alert ('hellohiroki ' + '!');
+}
+function mako65() {
+    alert ('hellomako ' + '!');
+}
