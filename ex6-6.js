1bac7d3e332043742e69bc8f0b69f22d8c016822
hirokinose <zm18227@josai.ac.jp>
2019-12-10 03:40:20 +0000

    12/10

---
+++ ex6-6.js
@@ -0,0 +1,15 @@
+var who66 = 'world';
+// var come = 'が来た'
+function sayhello66() {
+// var who = 'world';
+   alert('Hello,' + who66 + '!');
+}
+
+function hiroki66() {
+    who66 = '博己' ;
+}
+function mako66() {
+    who66 = '真子' ;
+}
+function gakita() {alert(who66 + 'が来た');
+}
